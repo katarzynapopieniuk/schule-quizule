8fb72e257ce6f2da18a36430ddfeca5248a9abec
Wiktor <kriegerwiktor@gmail.com>
2022-11-06 17:28:03 +0100

    PreAlpha

--- index.php
+++ index.php
@@ -1,9 +1,51 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <title>Schule Quizule</title>
+    <meta charset="UTF-8">
+    <meta name=""viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
+<!-- Navbar -->
+<div class="top">
+    
+  <div class="top-left">
+    <a href="Quizule.html"> 
+    <img src="LOGO.png" width = "20%" height = "20%"></a> 
+        <a href="#">Temp</a>
+        <a href="#">Temp</a>
+        <a href="#">Temp</a>    
+  </div>
+
+  <nav class="top-right">
+    <div class="dropdown">
+        <a href="#">Logowanie</a>
+        <ul>
+            <li><a href="#">Zaloguj</a></li>
+            <li><a href="#">Zarejestruj</a></li>
+        </ul>
+    </div>
+  </nav>
 </div>
-</body>
-</html>
+
+<!-- Sidebar -->
+<div class="left-column">
+    <nav class="topics-menu">
+        <a href="#">Polski</a>
+        <a href="#">Matematyka</a>
+        <a href="#">Przyroda</a>
+        <a href="#">Angielski</a>
+
+    </nav>
+</div>
+
+<div class="main" style="margin-left:250px">
+    
+
+</div>
+
+    <div class="footer">
+      <h4>Footer</h4>
+    </div>
